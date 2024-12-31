31c731a01457afbbd63a82fadf6f18bd361b5fe2
Andrii Ka <80795523+andriikali@users.noreply.github.com>
2024-12-31 14:35:46 -0800

    Initial Python-based Sandbox code (#46)
    Co-authored-by: Sally Lee <sally.lee@uber.com>

--- python/michelangelo/cli/cli.py
+++ python/michelangelo/cli/cli.py
@@ -1,13 +1,32 @@
 import sys
 import argparse
+import logging
+from michelangelo.cli.sandbox import sandbox
+
+description = """
+Michelangelo CLI is a command-line interface that enables seamless access to Michelangelo services directly from your terminal.
+"""
 
 
 def main(args=None):
-    p = argparse.ArgumentParser(description="Michelangelo CLI")
-    p.add_argument("target", choices=["sandbox"])
+    logging.basicConfig(level=logging.INFO)
+
+    p = argparse.ArgumentParser(description=description)
+    sp = p.add_subparsers(dest="entity", required=True, help="Entity to operate on")
+
+    sandbox_p = sp.add_parser(
+        "sandbox",
+        description=sandbox.description,
+        help=sandbox.short_description,
+    )
+    sandbox.init_arguments(sandbox_p)
+
     ns = p.parse_args(args=args)
 
-    print(ns)
+    if ns.entity == "sandbox":
+        return sandbox.run(ns)
+
+    raise ValueError("Unsupported entity: %s" % ns.entity)
 
 
 if __name__ == "__main__":
---
+++ python/michelangelo/cli/sandbox/README.md
@@ -0,0 +1,22 @@
+Sandbox is a lightweight version of the Michelangelo cluster, designed specifically for development and testing. 
+It also serves as an excellent tool for users to quickly explore the platform and familiarize themselves with its interface.
+
+> Note: The Sandbox deployment is intended for development and testing purposes only and is not suitable for production environments.
+> For guidance on creating a production-ready Michelangelo deployment, please refer to the Deployment Guide.
+
+## User Guide
+
+### Prerequisites
+
+Please install the following software before proceeding:
+
+- [Docker](https://docs.docker.com/get-started/get-docker)
+- [kubectl](https://kubernetes.io/docs/tasks/tools/#kubectl)
+- [k3d](https://k3d.io)
+
+### Install
+
+```
+pip install michelangelo
+ma sandbox --help
+```
