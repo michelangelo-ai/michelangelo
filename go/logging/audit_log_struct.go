9633e63016adb4501c2b9e37409cb8cdfcbb3f59
Ying Zheng <yingz@uber.com>
2024-12-30 18:03:20 -0800

    Refactor and cleanup logging

--- go/logging/audit_log_struct.go
+++ go/logging/audit_log_struct.go
@@ -5,16 +5,8 @@ type AuditLogEvent struct {
 	EntityType string
 	EntityName string
 	Procedure  string
-	UserEmail  string
-	Source     string
-	Headers    string
 	Request    string
 	Response   string
 	Error      string
-	MAUUID     string
-	RuntimeEnv string
-	CRDDiff    string
 	PreCRD     string
-	TimeStamp  int64
-	Dimension  string
 }
---
+++ go/logging/constants.go
@@ -0,0 +1,13 @@
+package logging
+
+// MichelangeloAPIScopeName is the metrics sub-scope name for all Michelangelo API calls
+const MichelangeloAPIScopeName = "ma_api"
+
+// APIProcedureTag is the metrics tag key for yarpc procedure name
+const APIProcedureTag = "api_procedure"
+
+// NamespaceTag is the metrics tag key for Michelangelo project name
+const NamespaceTag = "namespace"
+
+// EntityNameTag is the Tally metrics tag key for entity name
+const EntityNameTag = "name"
