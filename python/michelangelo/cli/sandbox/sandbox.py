9d1d8f9c35577d39194466552a411d7c65f56910
andrii <andrii@uber.com>
2025-01-30 09:45:16 -0800

    enable container registry auth for sandbox

--- python/michelangelo/cli/sandbox/sandbox.py
+++ python/michelangelo/cli/sandbox/sandbox.py
@@ -1,6 +1,9 @@
+import json
+import os
 import sys
 import argparse
 import subprocess
+import tempfile
 import time
 import uuid
 from pathlib import Path
@@ -28,8 +31,7 @@ def init_arguments(p: argparse.ArgumentParser):
     sp = p.add_subparsers(dest="action", required=True)
 
     create_p = sp.add_parser("create", help="Create and start the cluster.")
-    # TODO: andrii: Implement the --exclude option in the sandbox CLI.
-    create_p.add_argument("--exclude", help="Excludes the specified services.", nargs="+")
+    create_p.add_argument("--exclude", help="Excludes the specified services.", nargs="+", default=[])
 
     _ = sp.add_parser("delete", help="Delete the cluster.")
     _ = sp.add_parser("start", help="Start the cluster.")
@@ -44,6 +46,10 @@ def main(args=None):
 
 
 def run(ns: argparse.Namespace):
+    # Assert prerequisites. Sandbox depends on the following tools:
+    _assert_command("k3d", "k3d not found, please install it: https://k3d.io")
+    _assert_command("kubectl", "kubectl not found, please install it: https://kubernetes.io/docs/tasks/tools/#kubectl")
+
     if ns.action == "create":
         return _create(ns)
     if ns.action == "delete":
@@ -62,10 +68,55 @@ def _create(ns: argparse.Namespace):
     for p in _kube_ports:
         args += ["-p", f"{p}@agent:0"]
 
+    # TODO: andrii: Remove the following block once Michelangelo is publicly accessible.
+    # BLOCK START ----------------------------------------------------------------------
+    # Handle the GitHub Container Registry authentication.
+    env_cr_pat = "CR_PAT"
+    cr_pat = os.environ.get(env_cr_pat)
+    if not cr_pat:
+        _err_exit(
+            """
+CR_PAT environment variable is not set. To pull Michelangelo's containers from the GitHub Container Registry, please create a GitHub personal access token (classic) with the "read:packages" scope. Then, save this token to the CR_PAT environment variable, e.g.: `export CR_PAT=ghp_...`.
+
+For a detailed guide, check https://docs.github.com/en/packages/working-with-a-github-packages-registry/working-with-the-container-registry#authenticating-with-a-personal-access-token-classic.
+
+Be aware that CR_PAT environment variable is required while Michelangelo is NOT publicly accessible. Once we become public, the token will no longer be necessary, and this assertion will be removed.
+"""
+        )
+
+    # Create a temporary registry file with the GitHub Container Registry authentication.
+    registry = {
+        "mirrors": {
+            "ghcr.io": {
+                "endpoint": ["https://ghcr.io"],
+            },
+        },
+        "configs": {
+            "ghcr.io": {
+                "auth": {
+                    "username": "USERNAME",
+                    "password": cr_pat,
+                },
+            },
+        },
+    }
+
+    registry_file = tempfile.NamedTemporaryFile(mode="wt")
+    json.dump(registry, registry_file)
+    registry_file.flush()
+
+    args += ["--registry-config", registry_file.name]
+
+    # BLOCK END ----------------------------------------------------------------------
+
     _exec(*args)
 
-    for f in ["boot.yaml", "mysql.yaml", "cadence.yaml"]:
-        _kube_create(_dir / "resources" / f)
+    resources = ["boot.yaml", "mysql.yaml", "cadence.yaml"]
+    if "worker" not in ns.exclude:
+        resources.append("michelangelo-worker.yaml")
+
+    for r in resources:
+        _kube_create(_dir / "resources" / r)
 
     _exec("kubectl", "create", "-k", "github.com/ray-project/kuberay/ray-operator/config/default?ref=v1.2.2")
 
@@ -146,16 +197,63 @@ def _exec(
     *args,
     retry_attempts: int = 0,
     retry_delay_seconds: int = 5,
-) -> int:
+    raise_error: bool = False,
+):
+    """
+    Execute a shell command with optional retries. If the command exits with a non-zero code, it will be retried up to
+    retry_attempts times, waiting retry_delay_seconds between attempts.
+
+    Parameters:
+        *args: Variable-length argument list representing the command to run and its arguments.
+        retry_attempts: Number of times to retry the command on failure. Defaults to 0 (no retry).
+        retry_delay_seconds: Number of seconds to wait between retries. Defaults to 5.
+        raise_error: Determines how to handle errors after the final retry. If True, the function will raise a
+            subprocess.CalledProcessError. If False, the function will terminate the program with the exit code of the
+            failed command. Defaults to False.
+
+    Returns:
+        None.
+
+    Raises:
+        subprocess.CalledProcessError: If the command fails after all retries and raise_error is True.
+
+    Examples:
+        - Basic usage with a single command: _exec("ls", "-l", "~/bin")
+        - Run a script with retries: _exec("bash", "my_script.sh", retry_attempts=3, retry_delay_seconds=2)
+
+    Side Effects:
+        - Prints the command being executed and retry messages if any.
+        - Terminates the program if raise_error is False and retries are exhausted.
+    """
     for i in range(retry_attempts + 1):
         try:
             print("[+]", " ".join(args))
-            return subprocess.check_call(args)
-        except Exception as e:
+            subprocess.check_call(args)
+            return
+        except subprocess.CalledProcessError as e:
             if i == retry_attempts:
-                raise e
+                # This was the last attempt, either re-raise or exit.
+                if raise_error:
+                    raise e
+                else:
+                    _err_exit("command failed", code=e.returncode)
+
+            # Wait before the next attempt.
             print("retrying after", retry_delay_seconds, "seconds...")
             time.sleep(retry_delay_seconds)
+
+
+def _assert_command(command: str, err_message: str):
+    try:
+        _exec("command", "-v", command, raise_error=True)
+    except subprocess.CalledProcessError:
+        _err_exit(err_message)
+
+
+def _err_exit(err_message: str, code: int = 1):
+    # Print the error message in red and bold.
+    print(f"\033[91m\033[1mERROR: {err_message}\nexit {code}\033[0m")
+    sys.exit(code)
 
 
 if __name__ == "__main__":
