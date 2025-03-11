44b043c9ac6ebf2b628db358cee0a662b9e78822
Sally Lee <sally.lee@uber.com>
2025-03-11 12:53:33 -0700

    Add apiserver and controllermgr in sandbox (#109)
    * change api server port and print configuration
    
    * Add apiserver and controllermgr in sandbox
    
    * Change localhost to container name, increase timeout
    
    * Remove api_run
    
    * Uncomment for apiserver and minio
    
    * Add image build in bazel
    
    ---------
    
    Co-authored-by: Ying Zheng <yingz@uber.com>

--- python/michelangelo/cli/sandbox/sandbox.py
+++ python/michelangelo/cli/sandbox/sandbox.py
@@ -117,14 +117,18 @@ Be aware that CR_PAT environment variable is required while Michelangelo is NOT 
     resources = ["boot.yaml", "mysql.yaml", "cadence.yaml", "minio.yaml", "michelangelo-config.yaml"]
     if "worker" not in ns.exclude:
         resources.append("michelangelo-worker.yaml")
+    if "apiserver" not in ns.exclude:
+        resources.append("michelangelo-apiserver.yaml")
+    if "controllermgr" not in ns.exclude:
+        resources.append("michelangelo-controllermgr.yaml")
 
     for r in resources:
         _kube_create(_dir / "resources" / r)
 
     _exec("kubectl", "create", "-k", "github.com/ray-project/kuberay/ray-operator/config/default?ref=v1.2.2")
 
-    _exec("kubectl", "wait", "--all", "pods", "--for=condition=ready", "--timeout=300s")
-    _exec("kubectl", "-n", "ray-system", "wait", "--all", "deployments", "--for=condition=available", "--timeout=300s")
+    _exec("kubectl", "wait", "--all", "pods", "--for=condition=ready", "--timeout=600s")
+    _exec("kubectl", "-n", "ray-system", "wait", "--all", "deployments", "--for=condition=available", "--timeout=600s")
 
     links = []
 
