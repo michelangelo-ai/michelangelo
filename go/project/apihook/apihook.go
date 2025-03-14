2ed64bc989a67edb267fc3aeb240097b6fe9d112
ying-zheng <14048523+ying-zheng@users.noreply.github.com>
2025-03-14 13:29:16 -0700

    allow multiple api hooks (#113)
    * allow multiple api hooks

---
+++ go/project/apihook/apihook.go
@@ -0,0 +1,94 @@
+package apihook
+
+import (
+	"context"
+	"strings"
+
+	"github.com/michelangelo-ai/michelangelo/go/api"
+	v2 "github.com/michelangelo-ai/michelangelo/proto/api/v2"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	corev1 "k8s.io/api/core/v1"
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	k8sCoreClient "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/rest"
+)
+
+const (
+	_defaultNamespace         = "default"
+	_integrationTestNamespace = "ma-integration-test"
+	_systemNamespacePrefix    = "kube-"
+)
+
+type FxProjectAPIHookResult struct {
+	fx.Out
+	APIHook v2.ProjectAPIHook `group:"apiHooks"`
+}
+
+// GetProjectAPIHook returns the API hook for Project
+func GetProjectAPIHook(logger *zap.Logger, apiHandler api.Handler, k8sRestConfig *rest.Config) (FxProjectAPIHookResult, error) {
+	k8sClient, err := k8sCoreClient.NewForConfig(k8sRestConfig)
+	if err != nil {
+		return FxProjectAPIHookResult{}, err
+	}
+	return FxProjectAPIHookResult{
+		APIHook: apiHook{
+			logger:     logger,
+			apiHandler: apiHandler,
+			k8sClient:  k8sClient,
+		},
+	}, nil
+}
+
+type apiHook struct {
+	v2.NoopProjectAPIHook
+	logger     *zap.Logger
+	apiHandler api.Handler
+	k8sClient  k8sCoreClient.CoreV1Interface
+}
+
+// BeforeCreate creates a new namespace of the same name before creating a project
+func (a apiHook) BeforeCreate(ctx context.Context, request *v2.CreateProjectRequest) error {
+	// Validate the request
+	if request.Project.Namespace != _integrationTestNamespace && request.Project.Name != request.Project.Namespace {
+		return status.Errorf(codes.InvalidArgument,
+			"project name <%s> is different from namespace name <%s>. Project name must be the same as namespace name.",
+			request.Project.Name,
+			request.Project.Namespace)
+	}
+
+	if request.Project.Namespace == _defaultNamespace || strings.HasPrefix(request.Project.Namespace, _systemNamespacePrefix) {
+		return status.Errorf(codes.PermissionDenied,
+			"namespace <%s> is invalid. Users are forbidden to create projects in default or system namespace",
+			request.Project.Namespace)
+	}
+
+	// Create namespace
+	namespace := corev1.Namespace{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "namespace",
+			APIVersion: "core/v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: request.Project.Namespace,
+		},
+		Spec: corev1.NamespaceSpec{},
+	}
+	resp, err := a.k8sClient.Namespaces().Create(ctx, &namespace, metav1.CreateOptions{})
+
+	// if namespace already exist, we continue with project creation
+	if k8sErrors.IsAlreadyExists(err) {
+		a.logger.Info("Namespace already exists.")
+		return nil
+	}
+	if err != nil {
+		a.logger.Error("Fail to create namespace", zap.Error(err))
+		return api.K8sError2GrpcError(err, "failed to create namespace")
+	}
+
+	a.logger.Info("Successfully create namespace", zap.String("namespace", request.Project.Namespace), zap.Any("response", resp))
+	return nil
+}
---
+++ go/project/apihook/apihook_test.go
@@ -0,0 +1,174 @@
+package apihook
+
+import (
+	"context"
+	"testing"
+
+	v2 "github.com/michelangelo-ai/michelangelo/proto/api/v2"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	fakek8sclient "k8s.io/client-go/kubernetes/fake"
+	k8sCoreClient "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/rest"
+	k8stesting "k8s.io/client-go/testing"
+)
+
+func TestCreateProject(t *testing.T) {
+	request := &v2.CreateProjectRequest{
+		Project: &v2.Project{
+			ObjectMeta: v1.ObjectMeta{
+				Name:      "test",
+				Namespace: "test",
+			},
+			Spec: v2.ProjectSpec{
+				Tier:        2,
+				Description: "unit test project",
+				RootDir:     "/test-dir",
+				Owner: &v2.OwnerInfo{
+					OwningTeam: "1234",
+				},
+			},
+		},
+	}
+
+	testCases := []struct {
+		name          string
+		expectedError bool
+		req           *v2.CreateProjectRequest
+		setup         func(client k8sCoreClient.CoreV1Interface, clientset *fakek8sclient.Clientset)
+		assert        func(t *testing.T, err error, client k8sCoreClient.CoreV1Interface)
+	}{
+		{
+			name:  "create new namespace",
+			req:   request,
+			setup: nil,
+			assert: func(t *testing.T, err error, client k8sCoreClient.CoreV1Interface) {
+				assert.NoError(t, err)
+				list, err := client.Namespaces().List(context.Background(), v1.ListOptions{})
+				assert.NoError(t, err)
+				assert.Len(t, list.Items, 1)
+				assert.Equal(t, "test", list.Items[0].Name)
+			},
+		},
+		{
+			name: "create a project with existing namespace",
+			req:  request,
+			setup: func(client k8sCoreClient.CoreV1Interface, _ *fakek8sclient.Clientset) {
+				namespace := corev1.Namespace{
+					TypeMeta: metav1.TypeMeta{
+						Kind:       "namespace",
+						APIVersion: "core/v1",
+					},
+					ObjectMeta: metav1.ObjectMeta{
+						Name: request.Project.Namespace,
+					},
+					Spec: corev1.NamespaceSpec{},
+				}
+				_, err := client.Namespaces().Create(context.Background(), &namespace, v1.CreateOptions{})
+				assert.NoError(t, err)
+			},
+			assert: func(t *testing.T, err error, client k8sCoreClient.CoreV1Interface) {
+				assert.NoError(t, err)
+				list, err := client.Namespaces().List(context.Background(), v1.ListOptions{})
+				assert.NoError(t, err)
+				assert.Len(t, list.Items, 1)
+				assert.Equal(t, "test", list.Items[0].Name)
+			},
+		},
+		{
+			name: "failed to create namespace",
+			req:  request,
+			setup: func(_ k8sCoreClient.CoreV1Interface, clientset *fakek8sclient.Clientset) {
+				clientset.PrependReactor("create", "namespaces", func(action k8stesting.Action) (handled bool, ret runtime.Object, err error) {
+					return true, nil, assert.AnError
+				})
+			},
+			assert: func(t *testing.T, err error, client k8sCoreClient.CoreV1Interface) {
+				assert.Error(t, err, "failed to create namespace: assert.AnError general error for testing")
+			},
+		},
+		{
+			name:          "create project with a name different from namespace throws error",
+			expectedError: true,
+			req: &v2.CreateProjectRequest{
+				Project: &v2.Project{
+					ObjectMeta: v1.ObjectMeta{
+						Name:      "test_project",
+						Namespace: "test",
+					},
+					Spec: v2.ProjectSpec{
+						Tier:        2,
+						Description: "unit test project",
+						RootDir:     "/test-dir",
+						Owner: &v2.OwnerInfo{
+							OwningTeam: "1234",
+						},
+					},
+				},
+			},
+			setup: nil,
+			assert: func(t *testing.T, err error, client k8sCoreClient.CoreV1Interface) {
+				assert.Error(t, err)
+				assert.ErrorContains(t, err,
+					"project name <test_project> is different from namespace name <test>. Project name must be the same as namespace name.")
+			},
+		},
+		{
+			name:          "create project with name = 'default' throws error",
+			expectedError: true,
+			req: &v2.CreateProjectRequest{
+				Project: &v2.Project{
+					ObjectMeta: v1.ObjectMeta{
+						Name:      "default",
+						Namespace: "default",
+					},
+					Spec: v2.ProjectSpec{
+						Tier:        2,
+						Description: "unit test project",
+						RootDir:     "/test-dir",
+						Owner: &v2.OwnerInfo{
+							OwningTeam: "1234",
+						},
+					},
+				},
+			},
+			setup: nil,
+			assert: func(t *testing.T, err error, client k8sCoreClient.CoreV1Interface) {
+				assert.Error(t, err)
+				assert.ErrorContains(t, err,
+					"namespace <default> is invalid. Users are forbidden to create projects in default or system namespace")
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			clientSet := fakek8sclient.NewClientset()
+			client := clientSet.CoreV1()
+			fakeAPIHook := apiHook{
+				logger:     zap.NewNop(),
+				apiHandler: nil,
+				k8sClient:  client,
+			}
+
+			if tc.setup != nil {
+				tc.setup(client, clientSet)
+			}
+			err := fakeAPIHook.BeforeCreate(context.Background(), tc.req)
+			tc.assert(t, err, client)
+		})
+	}
+}
+
+func TestGetProjectAPIHook(t *testing.T) {
+	logger := zap.NewNop()
+	k8sRestConfig := &rest.Config{}
+	result, err := GetProjectAPIHook(logger, nil, k8sRestConfig)
+	assert.NoError(t, err)
+	assert.NotNil(t, result)
+	assert.NotNil(t, result.APIHook)
+}
