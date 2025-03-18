e3e5ccaa503a20e2e856cd7fe7cdb2eaca781c76
Craig Marker <craig.marker@uber.com>
2025-03-18 12:36:13 -0700

    Generate vite application for Michelangelo UI
    vite application was generated for Michelangelo UI using yarn create
    vite. Removed much of the boilerplate from App.tsx,css and index.css.
    
    Leveraging yarn workspaces, following the multi-package model outlined
    in the open source design document. the app/ directory exists for
    testing locally.
    
    Used vite's recommendations for eslint configuration—adding
    recommendedTypeCheck and stylisticTypeCheck. Did not include
    strictTypeCheck because Michelangelo Studio does not use this
    internally, and it is only recommended for those proficient with
    TypeScript.
    
    Migrated the import eslint logic from Michelangelo Studio internal as
    well (simple-import-sort)
    
    Onboarded to prettier, leveraging the same configuration Michelangelo
    Studio uses internally.

---
+++ javascript/eslint.config.js
@@ -0,0 +1,51 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  { ignores: ['dist', 'michelangelo/gen'] },
+  {
+    extends: [
+      'prettier',
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            // Packages `react` related packages come first, then any external dependencies
+            ['^react', '^@?\\w'],
+
+            // Internal imports
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$', '^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+
+            //Type imports
+            ['^@?\\w.*\\u0000$', '^[^.].*\\u0000$', '^\\..*\\u0000$'],
+          ],
+        },
+      ],
+    },
+  }
+);
