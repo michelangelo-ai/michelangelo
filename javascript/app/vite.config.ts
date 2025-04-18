5a5370f2eb89abbb47fcb1708eb8b2473c70927a
Craig Marker <craig@marker.org>
2025-04-18 12:45:34 -0700

    Remove src/ directories

--- javascript/app/vite.config.ts
+++ javascript/app/vite.config.ts
@@ -1,7 +1,6 @@
 import react from '@vitejs/plugin-react';
-
-import { defineConfig } from 'vite';
 import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: __dirname, // 👈 This ensures Vite serves from javascript/app
@@ -11,8 +10,8 @@ export default defineConfig({
       // Multiple aliases are required for the core package, such that
       // the core package can import from itself using "@" and app can
       // import from the core package using "@michelangelo/core"
-      '@': path.resolve(__dirname, '../packages/core/src'),
-      '@michelangelo/core': path.resolve(__dirname, '../packages/core/src'),
+      '@': path.resolve(__dirname, '../packages/core'),
+      '@michelangelo/core': path.resolve(__dirname, '../packages/core'),
 
       '@ma/gen-k8s': path.resolve(__dirname, '../gen/grpc/k8s.io/'),
       '@ma/gen-api': path.resolve(__dirname, '../gen/grpc/michelangelo/api/'),
