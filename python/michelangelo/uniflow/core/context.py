c8fc2e0ef07391d10a81e4c8d6e9fb5a95dd9f86
weric <weric@uber.com>
2025-04-22 13:29:41 -0700

    fix temporal

--- python/michelangelo/uniflow/core/context.py
+++ python/michelangelo/uniflow/core/context.py
@@ -14,6 +14,8 @@ from michelangelo.uniflow.core.remote_run import (
 from michelangelo.uniflow.core.utils import LOGGING_FORMAT, ArgparseEnvironAction
 
 log = logging.getLogger(__name__)
+cadence = "cadence"
+temporal = "temporal"
 
 
 @dataclass(frozen=True)
@@ -130,6 +132,11 @@ def _remote_run_argument_parser(environ=False) -> argparse.ArgumentParser:
 
     p = argparse.ArgumentParser()
     p.add_argument(
+        "--workflow",
+        default=cadence,
+        help="The workflow engine to use for remote execution. Options: cadence, temporal. Default is cadence.",
+    )
+    p.add_argument(
         "--storage-url",
         required=True,
         help="Persistent storage URL for saving and loading workflow checkpoints.",
@@ -172,6 +179,7 @@ def _remote_run(
     storage_url: str = "",
     image: str = "",
     yes: bool = False,
+    workflow: str = cadence,
 ):
     """
     Execute a given workflow function in Remote Mode.
@@ -196,11 +204,18 @@ def _remote_run(
 
     assert isinstance(environ, dict)
 
-    rr = RemoteRunTemporal(
-        fn=fn,
-        image=image,
-        storage_url=storage_url,
-    )
+    if workflow == cadence:
+        rr = RemoteRun(
+            fn=fn,
+            image=image,
+            storage_url=storage_url,
+        )
+    elif workflow == temporal:
+        rr = RemoteRunTemporal(
+            fn=fn,
+            image=image,
+            storage_url=storage_url,
+        )
     rr.environ = environ
     rr.args = args
     rr.kwargs = kwargs
