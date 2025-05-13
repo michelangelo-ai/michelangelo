502e138f5046ecebbb7baaf1fe5d129b8fbfde8d
hongw <hongw@uber.com>
2025-05-13 16:26:47 -0700

    import path

--- python/michelangelo/lib/model_manager/_private/packager/spark/__init__.py
+++
@@ -1,8 +0,0 @@
-# flake8: noqa:F401
-from .model_jar import create_model_jar
-from .model_zip import create_model_zip
-from .sample_data import create_sample_data_csv
-from .model_metadata import generate_model_metadata_content
-from .model_package import generate_model_package_content
-from .project_yaml import generate_project_yaml
-from .trained_model_yaml import generate_trained_model_yaml
--- python/michelangelo/lib/model_manager/packager/spark/__init__.py
+++
@@ -1,2 +0,0 @@
-# flake8: noqa:F401
-from michelangelo.lib.model_manager.packager.spark.spark_model_packager import SparkModelPackager
