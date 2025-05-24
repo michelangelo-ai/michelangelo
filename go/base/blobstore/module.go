6ffd85b6e9ed1ae2dca0cbb90742405e45e9e064
baoquan <baoquan@uber.com>
2025-05-24 08:37:41 -0700

    invoke minio module

--- go/base/blobstore/module.go
+++ go/base/blobstore/module.go
@@ -3,9 +3,11 @@ package blobstore
 import (
 	"go.uber.org/fx"
 	"go.uber.org/zap"
+	"github.com/michelangelo-ai/michelangelo/go/base/blobstore/minio"
 )
 
 var Module = fx.Options(
+	minio.Module,
 	fx.Provide(NewBlobStore),
 )
 
@@ -21,6 +23,7 @@ func NewBlobStore(in blobstoreClientsIn, logger *zap.Logger) *BlobStore {
 		logger:  logger.With(zap.String("component", "blobstore")),
 	}
 	for _, client := range in.BlobStoreClients {
+		logger.Info("Registering blobstore client", zap.String("scheme", client.Scheme()))
 		blobStore.RegisterClient(client)
 	}
 	return &blobStore
