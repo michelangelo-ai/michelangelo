3708cae48c6525f16225edfa97ec84768c7b6776
baoquan <baoquan@uber.com>
2025-05-24 16:48:43 -0700

    fix path scheme

--- go/base/blobstore/minio/client.go
+++ go/base/blobstore/minio/client.go
@@ -18,12 +18,12 @@ type minioClient struct {
 
 // Get retrieves an object from S3 storage, reads its content,
 // unmarshals the JSON data, and returns the result.
-// It expects the path format "bucket/filePath".
+// It expects the path format "/bucket/filePath".
 func (a *minioClient) Get(ctx context.Context, path string) (any, error) {
 	// Split the path into bucket and file path.
-	parts := strings.SplitN(path, "/", 2)
-	bucket := parts[0]
-	filePath := parts[1]
+	parts := strings.SplitN(path, "/", 3)
+	bucket := parts[1]
+	filePath := parts[2]
 
 	// Retrieve the object from the specified bucket and file path.
 	output, err := a.s3Client.GetObject(ctx, bucket, filePath, minio.GetObjectOptions{})
