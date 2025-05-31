4570424ceb0ba06d4886a0d0f877852fa47e8859
Craig Marker <craig@marker.org>
2025-05-31 10:54:55 -0700

    Implement icon system and update cell renderers
    Add Material UI and Emotion dependencies to support a new icon system. Create an
    IconProvider component and context to manage icons across the application. Update
    the BooleanCell component to use the new icon system with a circleCheckFilled
    icon. Update the project structure to support the new icon system by wrapping
    the application with IconProvider and exposing necessary components and types.

---
+++ javascript/app/icons.ts
@@ -0,0 +1,7 @@
+import CheckCircle from '@mui/icons-material/CheckCircle';
+import Launch from '@mui/icons-material/launch';
+
+export const ICONS = {
+  arrowLaunch: Launch,
+  circleCheckFilled: CheckCircle,
+} as const;
