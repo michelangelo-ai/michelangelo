9af5288433e340683c1144f004538ef380923800
Hwamin Kim <hwamin@uber.com>
2025-06-02 16:22:17 -0700

    Add Mactl prototype
    Apply command order
    
    Change CRD class name
    
    Add List function
    
    Add init files
    
    Update imports
    
    Add python path include for plugsins
    
    Separate out common functions
    
    separate out common functions
    
    Apply function with GET
    
    Add more loggings
    
    Add oss server configuration

---
+++ python/michelangelo/cli/mactl/plugins/pipeline/main.py
@@ -0,0 +1,24 @@
+from logging import getLogger
+
+from grpc import Channel
+
+from mactl import CRD
+from plugins.pipeline.apply import generate_apply
+from plugins.pipeline.create import generate_create
+
+
+_LOG = getLogger(__name__)
+
+
+def apply_plugins(
+    crd: CRD, target_command: str, crds: dict[str, CRD], channel: Channel
+):
+    """
+    Apply plugins to the crd.
+    """
+    _LOG.info("Applying plugins to crd: %r / %r", crd, target_command)
+    if target_command == "apply":
+        generate_apply(crd, channel)
+    if target_command == "create":
+        generate_create(crd, channel)
+    _LOG.info("Plugins applied successfully to crd: %s", crd)
