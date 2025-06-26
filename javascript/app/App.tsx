6c2be0358cc98ef0bb052866e9ba3400fd966997
Craig Marker <craig@marker.org>
2025-06-26 14:51:48 -0700

    Require QueryClientProvider from parent application

--- javascript/app/App.tsx
+++ javascript/app/App.tsx
@@ -5,6 +5,7 @@ import { Client as Styletron } from 'styletron-engine-atomic';
 import { Provider as StyletronProvider } from 'styletron-react';
 
 import { ICONS } from './icons/icons';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const dependencies = {
   theme: {
@@ -16,15 +17,18 @@ const dependencies = {
 };
 
 const engine = new Styletron();
+const queryClient = new QueryClient();
 
 export function App() {
   return (
     <StyletronProvider value={engine}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<CoreApp dependencies={dependencies} />} />
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/*" element={<CoreApp dependencies={dependencies} />} />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </StyletronProvider>
   );
 }
