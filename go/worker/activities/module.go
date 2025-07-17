4d4c3c159c8b87d74ea198432208816cf67d26e5
weric <weric@uber.com>
2025-07-17 13:38:15 +0800

    Fix

--- go/worker/activities/module.go
+++ go/worker/activities/module.go
@@ -2,17 +2,17 @@ package activities
 
 import (
 	"github.com/michelangelo-ai/michelangelo/go/worker/activities/cachedoutput"
-	"github.com/michelangelo-ai/michelangelo/go/cauldron/worker/activities/rayhttp"
-	"github.com/michelangelo-ai/michelangelo/go/cauldron/worker/activities/sparkhttp"
+	"github.com/michelangelo-ai/michelangelo/go/worker/activities/ray"
+	"github.com/michelangelo-ai/michelangelo/go/worker/activities/rayhttp"
+	"github.com/michelangelo-ai/michelangelo/go/worker/activities/spark"
 	"github.com/michelangelo-ai/michelangelo/go/worker/activities/storage"
 	"go.uber.org/fx"
 )
 
 var Module = fx.Options(
-	//ray.Module,
+	ray.Module,
 	rayhttp.Module,
-	//spark.Module,
-	sparkhttp.Module,
+	spark.Module,
 	storage.Module,
 	cachedoutput.Module,
 )
