397421516af6773fd86ee0eb8090a55820b33740
Craig Marker <99298045+craigmarker@users.noreply.github.com>
2025-09-02 16:04:42 -0700

    Fix issues during Execution merge back (#347)
    accessor is not a common task body schema property, as metadata renderer
    inlines accessor within cell. Explicitly added accessor (with expected
    output type) in each type and added explicit type for clarity and type
    safety.
    
    Task body renderer accepts interpolations at the task-level, for example
    metadata cells is an interpolator in Uber's instantiation of the
    pipeline run execution view.
    
    The fallback for task name was not engaging, since the getObjectValue
    was returning an empty string (non-falsy).

--- javascript/packages/core/components/views/execution/components/task-details/renderers/types.ts
+++ javascript/packages/core/components/views/execution/components/task-details/renderers/types.ts
@@ -1,7 +1,7 @@
 import type { RowCell } from '#core/components/row/types';
 import type { Accessor } from '#core/types/common/studio-types';
 
-export type TaskBodySchema = SharedTaskBodySchema | TaskBodyTextareaSchema | TaskBodyMetadataSchema;
+export type TaskBodySchema = TaskBodyStructSchema | TaskBodyTextareaSchema | TaskBodyMetadataSchema;
 
 export interface SharedTaskBodySchema {
   /**
@@ -12,6 +12,10 @@ export interface SharedTaskBodySchema {
   type: string;
 
   label: string;
+}
+
+export interface TaskBodyStructSchema extends SharedTaskBodySchema {
+  type: 'struct';
 
   /**
    * Used to access the value of the body content
@@ -19,22 +23,38 @@ export interface SharedTaskBodySchema {
    * @example 'spec.content.metadata.name'
    * @example (task) => task.input
    */
-  accessor: Accessor<unknown>;
+  accessor: Accessor<object>;
 }
 
 export interface TaskBodyTextareaSchema extends SharedTaskBodySchema {
+  type: 'textarea';
+
   error?: boolean;
   markdown?: boolean;
+
+  /**
+   * Used to access the value of the body content
+   *
+   * @example 'spec.content.metadata.name'
+   * @example (task) => task.input
+   */
+  accessor: Accessor<string>;
 }
 
 export interface TaskBodyMetadataSchema extends SharedTaskBodySchema {
+  type: 'metadata';
+
   cells: RowCell[];
+}
+
+export interface TaskBodyStructProps extends Omit<TaskBodyStructSchema, 'type' | 'accessor'> {
+  value?: object;
 }
 
 export interface TaskBodyTextAreaProps extends Omit<TaskBodyTextareaSchema, 'type' | 'accessor'> {
   value?: string;
 }
 
-export interface TaskBodyMetadataProps extends Omit<TaskBodyMetadataSchema, 'type' | 'accessor'> {
+export interface TaskBodyMetadataProps extends Omit<TaskBodyMetadataSchema, 'type'> {
   value?: Record<string, unknown>;
 }
