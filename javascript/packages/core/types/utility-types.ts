07e29c44d8cd4ed01d6009881e82ad53b12e097a
Craig Marker <craig@marker.org>
2025-09-15 16:54:57 -0700

    Fix ColumnConfig tooltip type
    Tooltips defined within table context now are typed correctly to accept
    row context.
    
    In order to continue the pattern of ColumnConfig extending Cell,
    leveraged distributed omit pattern provided by TypeScript. This ensures
    that ColumnConfig still has access to Cell types like MultiCellConfig,
    while also overwriting tooltip in all Cell types.

--- javascript/packages/core/types/utility-types.ts
+++ javascript/packages/core/types/utility-types.ts
@@ -8,3 +8,30 @@
 export type DeepPartial<T> = {
   [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P];
 };
+
+/**
+ * Distributive version of TypeScript's built-in `Omit` utility type.
+ *
+ * @description
+ * Unlike the standard `Omit<T, K>`, this type properly handles union types by
+ * distributing the omit operation across each member of the union. This is
+ * essential when working with discriminated unions where you need to remove
+ * a property from all variants.
+ *
+ * @example
+ * ```typescript
+ * // Standard Omit fails with unions:
+ * type BadExample = Omit<{ a: string; b: number } | { a: string; c: boolean }, 'a'>;
+ * // Result: {} (loses all other properties)
+ *
+ * // DistributiveOmit works correctly:
+ * type GoodExample = DistributiveOmit<{ a: string; b: number } | { a: string; c: boolean }, 'a'>;
+ * // Result: { b: number } | { c: boolean } (preserves other properties)
+ * ```
+ *
+ * @template T - The union type to omit properties from
+ * @template K - The keys to omit from each member of the union
+ *
+ * @see https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#distributive-conditional-types
+ */
+export type DistributiveOmit<T, K extends PropertyKey> = T extends unknown ? Omit<T, K> : never;
