0b66ab5cfcfafd37841d40d8adff2b9b0801b6bd
Craig Marker <craig@marker.org>
2025-09-16 14:06:30 -0700

    Support non-local deployment of Michelangelo UI

--- javascript/packages/rpc/services.ts
+++ javascript/packages/rpc/services.ts
@@ -20,7 +20,7 @@ const callerInterceptor: Interceptor = (next) => async (req) => {
 // This transport is used to connect to the Envoy proxy that proxies gRPC web requests
 // to gRPC services.
 const transport = createGrpcWebTransport({
-  baseUrl: 'http://localhost:8081',
+  baseUrl: 'http://envoy:8081',
   interceptors: [callerInterceptor],
   useBinaryFormat: true,
 });
