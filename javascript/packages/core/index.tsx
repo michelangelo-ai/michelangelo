842387d48f5af7d7667fd7e2d8705a60af97cb4f
Craig Marker <craig@marker.org>
2025-09-16 14:50:13 -0700

    Upgrade michelangelo UI for table fixes (#402)

--- javascript/packages/core/index.tsx
+++ javascript/packages/core/index.tsx
@@ -141,5 +141,6 @@ export type { RepeatedLayoutState } from '#core/providers/repeated-layout-provid
 export { Table } from '#core/components/table/table';
 export { useLocalStorageTableState } from '#core/components/table/plugins/state-persistence/use-local-storage-table-state';
 export { useTableSelectionContext } from '#core/components/table/plugins/selection/table-selection-context';
-export type { TableProps } from '#core/components/table/types/table-types';
 export { FilterMode } from '#core/components/table/components/filter/types';
+export type { ColumnConfig } from '#core/components/table/types/column-types';
+export type { TableProps } from '#core/components/table/types/table-types';
