0d52d7f7306ef1de617a28bf13518187b89c1284
yangfan0328 <susanzh211127@gmail.com>
2025-09-24 13:14:35 -0700

    Revert "import trigger run module"
    This reverts commit 175ef7c3c3e17a91514be63c46209515486acbab.

--- go/cmd/controllermgr/main.go
+++ go/cmd/controllermgr/main.go
@@ -18,7 +18,6 @@ import (
 	"github.com/michelangelo-ai/michelangelo/go/components/pipelinerun"
 	"github.com/michelangelo-ai/michelangelo/go/components/ray"
 	"github.com/michelangelo-ai/michelangelo/go/components/spark"
-	"github.com/michelangelo-ai/michelangelo/go/components/triggerrun"
 	"github.com/michelangelo-ai/michelangelo/go/controllermgr"
 	"github.com/michelangelo-ai/michelangelo/go/kubeproto/metrics"
 	v2pb "github.com/michelangelo-ai/michelangelo/proto/api/v2"
@@ -83,7 +82,6 @@ func options() fx.Option {
 		cadenceclient.Module,
 		pipeline.Module,
 		pipelinerun.Module,
-		triggerrun.Module,
 		controllermgr.Module,
 		deployment.Module,
 		fx.Invoke(func(logger *zap.Logger) {
