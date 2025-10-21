5c51d8f7f5468fa7f08d188f94f68efe4d3ce716
nikhila puppala <npuppala@uber.com>
2025-10-21 16:35:29 -0700

    removed clusterName from mactl.py and and create.py after rebase

--- python/michelangelo/cli/mactl/mactl.py
+++ python/michelangelo/cli/mactl/mactl.py
@@ -587,7 +587,6 @@ def convert_crd_metadata(
 
     res = {"spec": deepcopy(yaml_dict["spec"])}
     res["metadata"] = {
-        "clusterName": "",
         "generateName": "",
         "generation": "0",
         "name": yaml_dict["metadata"]["name"],
