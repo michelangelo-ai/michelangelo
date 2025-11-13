5a72e917e1f92349b5c1bdcc1dc775ff51eec50f
hongw <hongw@uber.com>
2025-11-13 12:26:44 -0800

    data type

--- python/michelangelo/lib/model_manager/schema/tests/data_type_test.py
+++ python/michelangelo/lib/model_manager/schema/tests/data_type_test.py
@@ -4,6 +4,5 @@ from michelangelo.lib.model_manager.schema.data_type import DataType
 
 class DataTypeTest(TestCase):
     def test_data_type(self):
-        # just to test that there is no malice in the DataType
         for data_type in DataType:
             self.assertIsInstance(data_type, DataType)
