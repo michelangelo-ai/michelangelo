7ee3700435c6c5b91b70d4cf201cace97e39b532
hongw <hongw@uber.com>
2025-11-13 15:04:05 -0800

    ok

--- python/michelangelo/lib/model_manager/packager/custom_triton/custom_triton_packager.py
+++ python/michelangelo/lib/model_manager/packager/custom_triton/custom_triton_packager.py
@@ -1,5 +1,5 @@
 import tempfile
-from typing import Optional
+from typing import Optional, Union
 from numpy import ndarray
 from michelangelo.lib.model_manager.schema import ModelSchema
 from michelangelo.lib.model_manager.constants import StorageType
@@ -51,9 +51,48 @@ class CustomTritonPackager:
                 modules with the given prefixes in the model package,
                 e.g. ['uber', 'data.michelangelo'] only imports starting
                 with 'uber' or 'data.michelangelo' will be saved in the
-                model package. Default is ['uber'],
+                model package.
                 and if the list is empty, save all imports
 
         Returns:
             The path of the model package
+        """
+
+    def create_raw_model_package(
+        self,
+        model_path: str,
+        model_class: str,
+        model_schema: ModelSchema,
+        sample_data: list[dict[str, ndarray]],
+        dest_model_path: Optional[str] = None,
+        model_path_source_type: Optional[str] = StorageType.LOCAL,
+        requirements: Optional[Union[list[str], str]] = None,
+        include_import_prefixes: Optional[list[str]] = None,
+    ) -> str:
+        """
+        Create a raw model package for custom Python model
+
+        Args:
+            model_path: the path of the raw model
+            model_class: the model class of the model
+                that contains the custom predict function
+            model_schema: the schema of the model, which specifies the input/palette/output features
+            sample_data: the sample data of the model. A list of input data for the predict function.
+            dest_model_path: the path to save the model package
+                If not specified, a temporary directory will be created
+            model_path_source_type: the source type of the model path,
+                e.g. 'hdfs', 'terrablob', default is 'hdfs'
+            requirements: the requirements of the model, which can be one of the following:
+                - a list of requirements
+                - a path to the requirements.txt file
+                If not specified, the requirements will not be included in the model package
+            include_import_prefixes (Optional): only save the imported
+                modules with the given prefixes in the model package,
+                e.g. ['uber', 'data.michelangelo'] only imports starting
+                with 'uber' or 'data.michelangelo' will be saved in the
+                model package.
+                and if the list is empty, save all imports
+
+        Returns:
+            The path of the raw model package
         """
