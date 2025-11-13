ec0b24d44e881f3234b410b4074fd03cd18a9827
hongw <hongw@uber.com>
2025-11-13 13:01:47 -0800

    update comment

--- python/michelangelo/lib/model_manager/packager/custom_triton/custom_triton_packager.py
+++ python/michelangelo/lib/model_manager/packager/custom_triton/custom_triton_packager.py
@@ -14,7 +14,7 @@ class CustomTritonPackager:
                 If to handle batch manually in the Triton model package.
                 Default is False. If set to True, the user is responsible for handling batch in the model class,
                 and the model input/output will have an additional batch dimension on top of the existing model schema.
-                For example, the schema shape [1] will be converted to [-1, 1].
+                For example, the schema shape [n, ..., m], the input dimension will be [batch_size, n, ..., m].
         """
         self.gen = TritonTemplateRenderer()
         self.custom_batch_processing = custom_batch_processing
