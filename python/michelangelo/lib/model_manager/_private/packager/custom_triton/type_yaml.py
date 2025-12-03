c6a76e259fc21861c46e8fa040bfd3d9c91c0f70
hongw <hongw@uber.com>
2025-12-03 12:49:23 -0800

    type yaml

---
+++ python/michelangelo/lib/model_manager/_private/packager/custom_triton/type_yaml.py
@@ -0,0 +1,17 @@
+import yaml
+from typing import Optional
+from michelangelo.lib.model_manager._private.constants import RawModelType
+
+
+def generate_type_yaml(batch_inference: Optional[bool] = False) -> str:
+    """Generate the type.yaml file content
+
+    Returns:
+        The type.yaml file content
+    """
+    content = {
+        "type": RawModelType.CUSTOM_PYTHON,
+    }
+    if batch_inference:
+        content["batch_inference"] = batch_inference
+    return yaml.dump(content, sort_keys=False)
