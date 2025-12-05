71affe31808252f43d32c510f50958d21f7ea467
hongw <hongw@uber.com>
2025-12-04 16:34:52 -0800

    serde for data encoder

---
+++ python/michelangelo/lib/model_manager/_private/serde/data/encoder.py
@@ -0,0 +1,17 @@
+"""Custom JSON encoder for model input/output data."""
+from json import JSONEncoder
+from numpy import ndarray
+
+
+class DataEncoder(JSONEncoder):
+    """Custom JSON encoder for model input/output data"""
+
+    def default(self, obj: any) -> any:
+        """Encode the object to a JSON-serializable dictionary."""
+        if isinstance(obj, ndarray):
+            return obj.tolist()
+
+        if isinstance(obj, bytes):
+            return obj.decode("utf-8")
+
+        return JSONEncoder.default(self, obj)
