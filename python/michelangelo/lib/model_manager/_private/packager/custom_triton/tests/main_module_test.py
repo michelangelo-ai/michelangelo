edd87730043fdcaefc7cf6f3b7534d4c087daa7d
Hong Wang <kennsmacintosh@gmail.com>
2025-12-04 16:14:11 -0800

    Model Manager: Add pickle dependency serialization support for custom Triton packager (#669)
    * pickled_model_binary
    
    * doc string
    
    * pickle
    
    * test
    
    * docstring
    
    * format
    
    * docstring
    
    * format
    
    * ok

---
+++ python/michelangelo/lib/model_manager/_private/packager/custom_triton/tests/main_module_test.py
@@ -0,0 +1,30 @@
+"""Tests for the main module serialization."""
+
+import os
+import tempfile
+from unittest import TestCase
+from unittest.mock import patch
+
+from michelangelo.lib.model_manager._private.packager.custom_triton import (
+    serialize_main_module,
+)
+
+
+class MainModuleTest(TestCase):
+    """Tests for the main module serialization."""
+
+    def test_serialize_main_module(self):
+        """Tests that the main module is serialized."""
+        with tempfile.TemporaryDirectory() as target_dir:
+            serialize_main_module(target_dir, include_import_prefixes=["michelangelo"])
+            self.assertTrue(len(os.listdir(target_dir)) > 0)
+
+    @patch(
+        "michelangelo.lib.model_manager._private.packager.custom_triton.main_module.inspect.getfile"
+    )
+    def test_serilize_main_module_skip(self, mock_getfile):
+        """Tests that the main module is not serialized if it is not a file."""
+        mock_getfile.side_effect = TypeError
+        with tempfile.TemporaryDirectory() as target_dir:
+            serialize_main_module(target_dir)
+            self.assertTrue(len(os.listdir(target_dir)) == 0)
