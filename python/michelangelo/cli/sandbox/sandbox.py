853c3d56f0e5eb010a1623f5cf7419e9e7451b2e
Aritra <ghosharitra@uber.com>
2025-12-08 13:38:33 -0800

    fix(sandbox): fix inference url endpoint

--- python/michelangelo/cli/sandbox/sandbox.py
+++ python/michelangelo/cli/sandbox/sandbox.py
@@ -1197,13 +1197,13 @@ def _create_inference_demo_crs():
         "🌐 Deployment-agnostic endpoint:\
             Use the following URL to test the inference server"
     )
-    print("  http://localhost:8889/inference-server-bert-cola/v2")
+    print("  http://localhost:8080/inference-server-bert-cola/v2")
     print(
         "  For example,\
             to test inference of a model deployed to the above inference server:\n"
     )
     print(
-        "  curl -X POST http://localhost:8889/inference-server-bert-cola/v2/models/<model-name>/infer \\"  # noqa: E501
+        "  curl -X POST http://localhost:8080/inference-server-bert-cola/v2/models/<model-name>/infer \\"  # noqa: E501
     )
     print('  -H "Content-Type: application/json" \\')
     print("  -d '{")
