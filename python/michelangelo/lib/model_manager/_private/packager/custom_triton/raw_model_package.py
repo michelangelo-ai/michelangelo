0cb6d98962ad9da1c7814a1d4ba1f5673070fc7f
Hong Wang <kennsmacintosh@gmail.com>
2025-12-09 13:15:04 -0800

    Model Manager: Enable Creating Raw Model Package with Custom Triton Packager  (#688)
    * raw model package
    
    * raw model package
    
    * ok
    
    * generate model package
    
    * assert
    
    * setup
    
    * fixture
    
    * debugging
    
    * fix test
    
    * add test
    
    * add test for validations
    
    * add test
    
    * add test
    
    * fix
    
    * import prefix
    
    * pickle
    
    * test add empty model
    
    * check
    
    * docstring
    
    * docstring
    
    * docstring
    
    * format
    
    * lint
    
    * fix lint
    
    * ruff

---
+++ python/michelangelo/lib/model_manager/_private/packager/custom_triton/raw_model_package.py
@@ -0,0 +1,121 @@
+"""Generate the raw model package content."""
+
+import os
+import tempfile
+from typing import Optional, Union
+
+from numpy import ndarray
+
+from michelangelo.lib.model_manager._private.packager.custom_triton.constants import (
+    MODEL_CLASS_FILE_NAME,
+)
+from michelangelo.lib.model_manager._private.packager.custom_triton.model_class import (
+    serialize_model_class,
+)
+from michelangelo.lib.model_manager._private.packager.custom_triton.pickled_model_binary import (  # noqa: E501
+    serialize_pickle_dependencies,
+)
+from michelangelo.lib.model_manager._private.packager.custom_triton.requirements_txt import (  # noqa: E501
+    generate_requirements_txt,
+)
+from michelangelo.lib.model_manager._private.packager.custom_triton.type_yaml import (
+    generate_type_yaml,
+)
+from michelangelo.lib.model_manager._private.schema.common import schema_to_yaml
+from michelangelo.lib.model_manager._private.serde.data import dump_model_data
+from michelangelo.lib.model_manager._private.utils.asset_utils import download_assets
+from michelangelo.lib.model_manager.constants import StorageType
+from michelangelo.lib.model_manager.schema import ModelSchema
+
+
+def generate_raw_model_package_content(
+    model_path: str,
+    model_class: str,
+    model_schema: ModelSchema,
+    sample_data: list[dict[str, ndarray]],
+    model_path_source_type: Optional[str] = StorageType.LOCAL,
+    requirements: Optional[Union[list[str], str]] = None,
+    root_path: Optional[str] = None,
+    include_import_prefixes: Optional[list[str]] = None,
+    batch_inference: Optional[bool] = False,
+) -> dict:
+    """Generate the raw model package content.
+
+    Args:
+        model_path: the path of the raw model
+        model_class: the model class of the model
+            that contains the custom predict function
+        model_schema: the schema of the model, which specifies the
+            input/palette/output features
+        sample_data: the sample data for the model. A list of input data
+            for the predict function.
+        model_path_source_type: the source type of the model path,
+            e.g. 'hdfs', 'terrablob', default is 'hdfs'
+        requirements: the requirements of the model, which can be one of the following:
+            - a list of requirements
+            - a path to the requirements.txt file
+            If not specified, the requirements will not be included in the model package
+        root_path (Optional): the root path for tmp files to be stored,
+            if not specified, use a temp dir
+        include_import_prefixes (Optional): only save the imported
+            modules with the given prefixes in the model package,
+            e.g. ['uber', 'data.michelangelo'] only imports starting
+            with 'uber' or 'data.michelangelo' will be saved in the
+            model package. Default is ['uber'],
+            and if the list is empty, save all imports
+        batch_inference (Optional): Specify if the prediction function in
+            the model class handles batch inference. Default is False.
+            If set to True, the model input/output will have an additional
+            batch dimension on top of the existing model schema.
+            For example, if the model schema specifies the input shape to be
+            [n, m], the model expects the input shape to be [-1, n, m].
+
+    Returns:
+        The raw model package content
+    """
+    if not root_path:
+        root_path = tempfile.mkdtemp()
+
+    target_model_path = os.path.join(root_path, "model")
+
+    os.makedirs(root_path, exist_ok=True)
+
+    download_assets(
+        model_path,
+        target_model_path,
+        model_path_source_type,
+    )
+
+    os.makedirs(target_model_path, exist_ok=True)
+
+    defs_path = os.path.join(root_path, "defs")
+
+    serialize_model_class(
+        model_class,
+        defs_path,
+        MODEL_CLASS_FILE_NAME,
+        include_import_prefixes=include_import_prefixes,
+    )
+
+    serialize_pickle_dependencies(
+        target_model_path,
+        defs_path,
+        include_import_prefixes=include_import_prefixes,
+    )
+
+    content = {
+        "metadata": {
+            "type.yaml": generate_type_yaml(batch_inference),
+            "schema.yaml": schema_to_yaml(model_schema),
+            "sample_data.json": dump_model_data(sample_data),
+        },
+        "model": f"dir://{target_model_path}",
+        "defs": f"dir://{defs_path}",
+    }
+
+    if requirements:
+        content["dependencies"] = {
+            "requirements.txt": generate_requirements_txt(requirements),
+        }
+
+    return content
--- python/michelangelo/lib/model_manager/packager/custom_triton/tests/custom_triton_packager_test.py
+++ python/michelangelo/lib/model_manager/packager/custom_triton/tests/custom_triton_packager_test.py
@@ -1,14 +1,409 @@
 """Tests for CustomTritonPackager."""
 
+import os
+import pickle
+import tempfile
+from pathlib import Path
 from unittest import TestCase
 
+import numpy as np
+
+from michelangelo.lib.model_manager._private.schema.common import schema_to_yaml
 from michelangelo.lib.model_manager.packager.custom_triton import CustomTritonPackager
+from michelangelo.lib.model_manager.packager.custom_triton.tests.fixtures.model import (
+    Model,
+)
+from michelangelo.lib.model_manager.schema import DataType, ModelSchema, ModelSchemaItem
+
+model_class = "michelangelo.lib.model_manager.packager.custom_triton.tests.fixtures.predict.Predict"  # noqa: E501
+model_class_with_relative_imports = (
+    "michelangelo.lib.model_manager.packager.custom_triton.tests.fixtures."
+    "predict_with_relative_import.Predict"
+)
 
 
 class CustomTritonPackagerTest(TestCase):
     """Tests instantiation of the custom Triton packager."""
 
+    def setUp(self):
+        """Set up the test environment."""
+        self.model_schema = ModelSchema(
+            input_schema=[
+                ModelSchemaItem(
+                    name="input",
+                    data_type=DataType.INT,
+                    shape=[1],
+                ),
+            ],
+            output_schema=[
+                ModelSchemaItem(
+                    name="response",
+                    data_type=DataType.INT,
+                    shape=[1],
+                ),
+            ],
+        )
+        self.sample_data = [{"input": np.array([1])}]
+        self.batch_sample_data = [{"input": np.array([[1], [2]])}]
+
     def test_custom_triton_packager(self):
         """It creates a packager instance with default settings."""
         packager = CustomTritonPackager()
         self.assertIsNotNone(packager)
+
+    def assert_raw_model_package(
+        self, dest_model_path, with_requirements=False, batch_inference=False
+    ):
+        """Assert that the raw model package has the expected structure."""
+        with open(os.path.join(dest_model_path, "defs", "model_class.txt")) as f:
+            content = f.read()
+            self.assertEqual(content, model_class)
+
+        with open(
+            os.path.join(
+                dest_model_path,
+                "defs",
+                "michelangelo",
+                "lib",
+                "model_manager",
+                "packager",
+                "custom_triton",
+                "tests",
+                "fixtures",
+                "predict.py",
+            ),
+        ) as f:
+            content = f.read()
+            self.assertIn("class Predict(Model):", content)
+
+        with open(os.path.join(dest_model_path, "model", "file.txt")) as f:
+            content = f.read()
+            self.assertEqual(content, "file_content")
+
+        with open(os.path.join(dest_model_path, "metadata", "type.yaml")) as f:
+            content = f.read()
+            if batch_inference:
+                self.assertEqual(
+                    content, "type: custom-python\nbatch_inference: true\n"
+                )
+            else:
+                self.assertEqual(content, "type: custom-python\n")
+
+        with open(os.path.join(dest_model_path, "metadata", "schema.yaml")) as f:
+            content = f.read()
+            self.assertEqual(content, schema_to_yaml(self.model_schema))
+
+        with open(os.path.join(dest_model_path, "metadata", "sample_data.json")) as f:
+            content = f.read()
+            if batch_inference:
+                self.assertEqual(content, '[{"input": [[1], [2]]}]')
+            else:
+                self.assertEqual(content, '[{"input": [1]}]')
+
+        files = sorted(
+            [
+                str(
+                    Path(os.path.join(dirpath, file)).relative_to(dest_model_path),
+                )
+                for dirpath, _, filenames in os.walk(dest_model_path)
+                for file in filenames
+            ],
+        )
+
+        expected_files = [
+            "defs/michelangelo/lib/model_manager/_private/utils/module_finder/tests/fixtures/folder/fn1.py",
+            "defs/michelangelo/lib/model_manager/_private/utils/module_finder/tests/fixtures/folder/fn2.py",
+            "defs/michelangelo/lib/model_manager/_private/utils/module_finder/tests/fixtures/folder/fn3.py",
+            "defs/michelangelo/lib/model_manager/_private/utils/module_finder/tests/fixtures/folder/fn4.py",
+            "defs/michelangelo/lib/model_manager/_private/utils/module_finder/tests/fixtures/package/__init__.py",
+            "defs/michelangelo/lib/model_manager/_private/utils/module_finder/tests/fixtures/package/fn1.py",
+            "defs/michelangelo/lib/model_manager/_private/utils/module_finder/tests/fixtures/package/fn2.py",
+            "defs/michelangelo/lib/model_manager/_private/utils/module_finder/tests/fixtures/simple_module.py",
+            "defs/michelangelo/lib/model_manager/interface/custom_model.py",
+            "defs/michelangelo/lib/model_manager/packager/custom_triton/tests/fixtures/predict.py",
+            "defs/model_class.txt",
+            "metadata/sample_data.json",
+            "metadata/schema.yaml",
+            "metadata/type.yaml",
+            "model/file.txt",
+        ]
+
+        if with_requirements:
+            expected_files.insert(-4, "dependencies/requirements.txt")
+
+        self.assertEqual(
+            files,
+            expected_files,
+        )
+
+    def test_create_raw_model_package(self):
+        """It creates a raw model package."""
+        packager = CustomTritonPackager()
+        with tempfile.TemporaryDirectory() as temp_dir:
+            model_path = os.path.join(temp_dir, "model")
+            dest_model_path = os.path.join(temp_dir, "raw_model")
+            os.makedirs(model_path)
+            with open(os.path.join(model_path, "file.txt"), "w") as f:
+                f.write("file_content")
+            dest_model_path = packager.create_raw_model_package(
+                model_path=model_path,
+                model_class=model_class,
+                model_schema=self.model_schema,
+                sample_data=self.sample_data,
+                dest_model_path=dest_model_path,
+                include_import_prefixes=["michelangelo"],
+            )
+            self.assert_raw_model_package(dest_model_path)
+
+    def test_create_raw_model_package_with_custom_batch_processing(self):
+        """It creates a raw model package with custom batch processing."""
+        packager = CustomTritonPackager(custom_batch_processing=True)
+        with tempfile.TemporaryDirectory() as temp_dir:
+            model_path = os.path.join(temp_dir, "model")
+            dest_model_path = os.path.join(temp_dir, "raw_model")
+            os.makedirs(model_path)
+            with open(os.path.join(model_path, "file.txt"), "w") as f:
+                f.write("file_content")
+            dest_model_path = packager.create_raw_model_package(
+                model_path=model_path,
+                model_class=model_class,
+                model_schema=self.model_schema,
+                sample_data=self.batch_sample_data,
+                dest_model_path=dest_model_path,
+                include_import_prefixes=["michelangelo"],
+            )
+            self.assert_raw_model_package(dest_model_path, batch_inference=True)
+
+    def test_create_raw_model_package_with_empty_model_schema(self):
+        """It creates a raw model package with empty model schema."""
+        packager = CustomTritonPackager()
+        with self.assertRaises(ValueError):
+            packager.create_raw_model_package(
+                "test_model_path",
+                model_class=model_class,
+                model_schema=None,
+                sample_data=self.sample_data,
+            )
+
+    def test_create_raw_model_package_with_invalid_model_schema(self):
+        """It creates a raw model package with invalid model schema."""
+        model_schema = ModelSchema(
+            input_schema=[
+                ModelSchemaItem(
+                    name="input",
+                    data_type=DataType.UNKNOWN,
+                ),
+            ],
+        )
+        packager = CustomTritonPackager()
+        with self.assertRaises(ValueError):
+            packager.create_raw_model_package(
+                "test_model_path",
+                model_class=model_class,
+                model_schema=model_schema,
+                sample_data=self.sample_data,
+            )
+
+    def test_create_raw_model_package_missing_model_class(self):
+        """It creates a raw model package with missing model class."""
+        with self.assertRaises(ValueError):
+            packager = CustomTritonPackager()
+            packager.create_raw_model_package(
+                "test_model_path",
+                model_class=None,
+                model_schema=self.model_schema,
+                sample_data=self.sample_data,
+            )
+
+    def test_create_raw_model_package_with_invalid_model_class(self):
+        """It creates a raw model package with invalid model class."""
+        with self.assertRaises(ValueError):
+            packager = CustomTritonPackager()
+            packager.create_raw_model_package(
+                "test_model_path",
+                model_class="invalid_class",
+                model_schema=self.model_schema,
+                sample_data=self.sample_data,
+            )
+
+    def test_create_raw_model_package_with_invalid_sample_data(self):
+        """It creates a raw model package with invalid sample data."""
+        with self.assertRaises(TypeError):
+            packager = CustomTritonPackager()
+            packager.create_raw_model_package(
+                "test_model_path",
+                model_class=model_class,
+                model_schema=self.model_schema,
+                sample_data=[{"a": "b"}],
+            )
+
+    def test_create_raw_model_package_with_mismatching_sample_data_and_model_schema(
+        self,
+    ):
+        """It creates a raw model package with mismatching sample data and schema."""
+        with self.assertRaises(ValueError):
+            packager = CustomTritonPackager()
+            packager.create_raw_model_package(
+                "test_model_path",
+                model_class=model_class,
+                model_schema=self.model_schema,
+                sample_data=[{"invalid": np.array([1])}],
+            )
+
+    def test_create_raw_model_package_with_requirements(self):
+        """It creates a raw model package with requirements."""
+        packager = CustomTritonPackager()
+        with tempfile.TemporaryDirectory() as temp_dir:
+            model_path = os.path.join(temp_dir, "model")
+            dest_model_path = os.path.join(temp_dir, "raw_model")
+            os.makedirs(model_path)
+            with open(os.path.join(model_path, "file.txt"), "w") as f:
+                f.write("file_content")
+            dest_model_path = packager.create_raw_model_package(
+                model_path=model_path,
+                model_class=model_class,
+                model_schema=self.model_schema,
+                sample_data=self.sample_data,
+                dest_model_path=dest_model_path,
+                requirements=["numpy", "pandas"],
+                include_import_prefixes=["michelangelo"],
+            )
+            self.assert_raw_model_package(dest_model_path, with_requirements=True)
+
+    def test_create_raw_model_package_with_default_dest_model_path(self):
+        """It creates a raw model package with default dest model path."""
+        packager = CustomTritonPackager()
+        with tempfile.TemporaryDirectory() as temp_dir:
+            model_path = os.path.join(temp_dir, "model")
+            os.makedirs(model_path)
+            with open(os.path.join(model_path, "file.txt"), "w") as f:
+                f.write("file_content")
+            dest_model_path = packager.create_raw_model_package(
+                model_path=model_path,
+                model_class=model_class,
+                model_schema=self.model_schema,
+                sample_data=self.sample_data,
+                include_import_prefixes=["michelangelo"],
+            )
+            self.assert_raw_model_package(dest_model_path)
+
+    def test_create_raw_model_package_with_altered_include_import_prefixes(self):
+        """It creates a raw model package with altered include import prefixes."""
+        packager = CustomTritonPackager()
+        with tempfile.TemporaryDirectory() as temp_dir:
+            model_path = os.path.join(temp_dir, "model")
+            dest_model_path = os.path.join(temp_dir, "raw_model")
+            os.makedirs(model_path)
+            with open(os.path.join(model_path, "file.txt"), "w") as f:
+                f.write("file_content")
+            dest_model_path = packager.create_raw_model_package(
+                model_path=model_path,
+                model_class=model_class,
+                model_schema=self.model_schema,
+                sample_data=self.sample_data,
+                dest_model_path=dest_model_path,
+                include_import_prefixes=[
+                    "michelangelo.lib.model_manager._private.utils.module_finder.tests.fixtures.package",
+                    "michelangelo.lib.model_manager._private.utils.module_finder.tests.fixtures.simple_module",
+                ],
+            )
+
+            files = sorted(
+                [
+                    str(
+                        Path(os.path.join(dirpath, file)).relative_to(dest_model_path),
+                    )
+                    for dirpath, _, filenames in os.walk(dest_model_path)
+                    for file in filenames
+                ],
+            )
+
+            self.assertEqual(
+                files,
+                [
+                    "defs/michelangelo/lib/model_manager/_private/utils/module_finder/tests/fixtures/package/__init__.py",
+                    "defs/michelangelo/lib/model_manager/_private/utils/module_finder/tests/fixtures/package/fn1.py",
+                    "defs/michelangelo/lib/model_manager/_private/utils/module_finder/tests/fixtures/package/fn2.py",
+                    "defs/michelangelo/lib/model_manager/_private/utils/module_finder/tests/fixtures/simple_module.py",
+                    "defs/michelangelo/lib/model_manager/interface/custom_model.py",
+                    "defs/michelangelo/lib/model_manager/packager/custom_triton/tests/fixtures/predict.py",
+                    "defs/model_class.txt",
+                    "metadata/sample_data.json",
+                    "metadata/schema.yaml",
+                    "metadata/type.yaml",
+                    "model/file.txt",
+                ],
+            )
+
+    def test_create_raw_model_package_with_pickle(self):
+        """It creates a raw model package with pickle."""
+        packager = CustomTritonPackager()
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            model_path = os.path.join(temp_dir, "model")
+            dest_model_path = os.path.join(temp_dir, "raw_model")
+            os.makedirs(model_path)
+            with open(os.path.join(model_path, "file.txt"), "w+") as f:
+                f.write("file_content")
+            with open(os.path.join(model_path, "file.pkl"), "wb") as f:
+                pickle.dump(Model(), f)
+            dest_model_path = packager.create_raw_model_package(
+                model_path=model_path,
+                model_class=model_class,
+                model_schema=self.model_schema,
+                sample_data=self.sample_data,
+                dest_model_path=dest_model_path,
+                include_import_prefixes=["michelangelo"],
+            )
+
+            files = sorted(
+                [
+                    str(
+                        Path(os.path.join(dirpath, file)).relative_to(dest_model_path),
+                    )
+                    for dirpath, _, filenames in os.walk(dest_model_path)
+                    for file in filenames
+                ],
+            )
+
+            self.assertEqual(
+                files,
+                [
+                    "defs/michelangelo/lib/model_manager/_private/utils/module_finder/tests/fixtures/folder/fn1.py",
+                    "defs/michelangelo/lib/model_manager/_private/utils/module_finder/tests/fixtures/folder/fn2.py",
+                    "defs/michelangelo/lib/model_manager/_private/utils/module_finder/tests/fixtures/folder/fn3.py",
+                    "defs/michelangelo/lib/model_manager/_private/utils/module_finder/tests/fixtures/folder/fn4.py",
+                    "defs/michelangelo/lib/model_manager/_private/utils/module_finder/tests/fixtures/package/__init__.py",
+                    "defs/michelangelo/lib/model_manager/_private/utils/module_finder/tests/fixtures/package/fn1.py",
+                    "defs/michelangelo/lib/model_manager/_private/utils/module_finder/tests/fixtures/package/fn2.py",
+                    "defs/michelangelo/lib/model_manager/_private/utils/module_finder/tests/fixtures/simple_module.py",
+                    "defs/michelangelo/lib/model_manager/interface/custom_model.py",
+                    "defs/michelangelo/lib/model_manager/packager/custom_triton/tests/fixtures/model.py",
+                    "defs/michelangelo/lib/model_manager/packager/custom_triton/tests/fixtures/predict.py",
+                    "defs/model_class.txt",
+                    "metadata/sample_data.json",
+                    "metadata/schema.yaml",
+                    "metadata/type.yaml",
+                    "model/file.pkl",
+                    "model/file.txt",
+                ],
+            )
+
+    def test_create_raw_model_package_with_empty_model(self):
+        """It creates a raw model package with empty model."""
+        packager = CustomTritonPackager()
+        with tempfile.TemporaryDirectory() as temp_dir:
+            model_path = os.path.join(temp_dir, "model")
+            dest_model_path = os.path.join(temp_dir, "raw_model")
+            os.makedirs(model_path)
+            dest_model_path = packager.create_raw_model_package(
+                model_path=model_path,
+                model_class=model_class,
+                model_schema=self.model_schema,
+                sample_data=self.sample_data,
+                dest_model_path=dest_model_path,
+                include_import_prefixes=["michelangelo"],
+            )
+
+            self.assertTrue(os.path.exists(os.path.join(dest_model_path, "model")))
+            self.assertEqual(len(os.listdir(os.path.join(dest_model_path, "model"))), 0)
