977bd0fce1337b692ea42237c943330ccc433a66
eric.w <eric.w@grabtaxi.com>
2025-12-09 12:38:50 -0800

    Add Azure client for blobstorage

--- go/base/blobstore/minio/module.go
+++ go/base/blobstore/minio/module.go
@@ -21,12 +21,21 @@ var Module = fx.Options(
 )
 
 // newClient initializes a new minioClient using the provided configuration.
-// It creates an underlying S3 client with static credentials.
+// It creates an underlying S3 client with the appropriate credentials.
 // Returns a pointer to minioClient or an error if initialization fails.
 func newClient(config Config) (BlobStoreClientOut, error) {
+	var creds *credentials.Credentials
+	if config.UseEnvAws {
+		creds = credentials.NewEnvAWS()
+	} else if config.UseIAM {
+		creds = credentials.NewIAM("")
+	} else {
+		creds = credentials.NewStaticV4(config.AwsAccessKeyId, config.AwsSecretAccessKey, "")
+	}
+
 	s3Client, err := minio.New(config.AwsEndpointUrl, &minio.Options{
-		Creds:  credentials.NewStaticV4(config.AwsAccessKeyId, config.AwsSecretAccessKey, ""),
-		Secure: false,
+		Creds:  creds,
+		Secure: true,
 	})
 	if err != nil {
 		return BlobStoreClientOut{}, err
