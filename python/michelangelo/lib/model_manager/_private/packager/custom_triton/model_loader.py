80d7c57f6e8898348b0126e245fe7d85bad46358
hongw <hongw@uber.com>
2025-12-11 11:37:14 -0800

    model loader

---
+++ python/michelangelo/lib/model_manager/_private/packager/custom_triton/model_loader.py
@@ -0,0 +1,16 @@
+from michelangelo.lib.model_manager._private.utils.module_finder import find_dependency_files
+from michelangelo.lib.model_manager._private.utils.module_utils import save_module_files
+from michelangelo.lib.model_manager._private.serde.loader.custom_model_loader import load_custom_model
+
+
+def serialize_model_loader(target_dir: str):
+    """Serialize the custom model loader.
+
+    Serialize the custom model loader and its dependencies to the target dir.
+    This function is only meant to be used when creating the deployable model package.
+
+    Args:
+        target_dir: The target dir to save the module files.
+    """
+    files = find_dependency_files(load_custom_model.__module__, prefixes=["michelangelo"])
+    save_module_files(files, target_dir)
