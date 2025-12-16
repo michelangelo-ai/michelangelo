c56b019e916e4b431a2507f37a8143115e83f8f2
hongw <hongw@uber.com>
2025-12-16 14:45:45 -0800

    test

--- python/michelangelo/lib/model_manager/_private/packager/custom_triton/tests/config_pbtxt_test.py
+++ python/michelangelo/lib/model_manager/_private/packager/custom_triton/tests/config_pbtxt_test.py
@@ -194,3 +194,48 @@ class ConfigPbtxtTest(TestCase):
             """
         )
         self.assertEqual(config_pbtxt, expected_config_pbtxt)
+
+    def test_generate_config_pbtxt_content_with_zero_name(self):
+        """It renders the expected config.pbtxt contents with model_name == 0."""
+        gen = TritonTemplateRenderer()
+        config_pbtxt = generate_config_pbtxt_content(
+            gen,
+            model_name=0,
+            model_revision=None,
+            input_schema={"input": {"data_type": "FP32", "shape": [1, 100]}},
+            output_schema={"output": {"data_type": "FP32", "shape": [1, 100]}},
+        )
+
+        expected_config_pbtxt = dedent(
+            """\
+            name: "0"
+            backend: "python"
+            max_batch_size: 256
+            dynamic_batching: {
+              preferred_batch_size: 10,
+              max_queue_delay_microseconds: 300,
+              preserve_ordering: true
+            }
+            input : [
+              {
+                name: "input",
+                data_type: TYPE_FP32,
+                dims: [1, 100]
+              }
+            ]
+            output: [
+              {
+                name: "output",
+                data_type: TYPE_FP32,
+                dims: [1, 100]
+              }
+            ]
+            instance_group: [
+              {
+                kind: KIND_CPU,
+                count: 1
+              }
+            ]
+            """
+        )
+        self.assertEqual(config_pbtxt, expected_config_pbtxt)
