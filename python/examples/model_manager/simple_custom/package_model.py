048b1a579fe1c23b48eb7487350e106706ca4804
hongw <hongw@uber.com>
2025-12-16 17:51:07 -0800

    fix command

--- python/examples/model_manager/simple_custom/package_model.py
+++ python/examples/model_manager/simple_custom/package_model.py
@@ -2,9 +2,7 @@
 
 Run from the `python/` directory:
 
-  PYTHONPATH="." poetry run python
-    ./examples/model_manager/simple_custom/package_model.py
-    --out /tmp/mm-simple-custom
+  PYTHONPATH="." poetry run python ./examples/model_manager/simple_custom/package_model.py --out /tmp/mm-simple-custom
 """
 
 from __future__ import annotations
