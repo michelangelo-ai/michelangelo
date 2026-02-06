221ef3e08fcf729b5a2ed8928e0bf4eb685d76c6
weric <weric@uber.com>
2026-02-06 10:57:30 -0800

    Fix unit test

--- go/base/config/config.go
+++ go/base/config/config.go
@@ -30,14 +30,14 @@ type K8sConfig struct {
 }
 
 type WorkflowClientConfig struct {
-	Service     string `yaml:"service"`
-	Host        string `yaml:"host"`
-	Transport   string `yaml:"transport"`
-	Domain      string `yaml:"domain"`
-	TaskList    string `yaml:"taskList"`
+	Service           string `yaml:"service"`
+	Host              string `yaml:"host"`
+	Transport         string `yaml:"transport"`
+	Domain            string `yaml:"domain"`
+	TaskList          string `yaml:"taskList"`
 	Provider  string `yaml:"provider"`
 	UseTLS    bool   `yaml:"useTLS"`
-	WorkflowUrlFormat string `yaml:"workflowUrlFormat"`
+	ExecutionUrlFormat string `yaml:"executionUrlFormat"`
 }
 
 // Params defines the dependencies of the config fx module.
