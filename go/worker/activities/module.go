b0a814bd38fb15aad5f32c63758b1fe6720422c1
Eric Wang <eric_8193@hotmail.com>
2026-02-20 04:38:06 +0800

    Move ray and spark plugins/activities into CMD instead of core modules (#838)
    * Move ray and spark plugins/activities into CMD instead of core modules
    
    * Move ray and spark plugins/activities into CMD instead of core modules
    
    * fix proto-go

--- go/worker/activities/module.go
+++ go/worker/activities/module.go
@@ -6,15 +6,11 @@ import (
 	"github.com/michelangelo-ai/michelangelo/go/worker/activities/cachedoutput"
 	"github.com/michelangelo-ai/michelangelo/go/worker/activities/model"
 	"github.com/michelangelo-ai/michelangelo/go/worker/activities/notification"
-	"github.com/michelangelo-ai/michelangelo/go/worker/activities/ray"
-	"github.com/michelangelo-ai/michelangelo/go/worker/activities/spark"
 	"github.com/michelangelo-ai/michelangelo/go/worker/activities/storage"
 	"github.com/michelangelo-ai/michelangelo/go/worker/activities/trigger"
 )
 
 var Module = fx.Options(
-	ray.Module,
-	spark.Module,
 	storage.Module,
 	model.Module,
 	cachedoutput.Module,
