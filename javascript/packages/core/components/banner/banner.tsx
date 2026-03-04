6d92fafa27d4f3f25360213482a3bbcac25a3e9c
Selim Konuk <selimkonuktr@gmail.com>
2026-03-04 10:16:55 +0100

    Add base banner component and banner layout (#870)
    * Add base banner component and banner layout
    
    * Add test case for Markdown support
    
    * Update jsdocs
    
    * Stop mergedOverride prop spreading
    
    ---------
    
    Co-authored-by: Selim Konuk <selim.konuk@uber.com>

---
+++ javascript/packages/core/components/banner/banner.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { mergeOverrides } from 'baseui';
+import { Banner as BaseBanner } from 'baseui/banner';
+
+import type { BannerProps } from 'baseui/banner';
+
+export const Banner: React.FC<BannerProps> = (props) => {
+  const { overrides = {}, children, ...rest } = props;
+
+  const mergedOverrides = mergeOverrides(overrides, {
+    Root: {
+      style: {
+        marginTop: 0,
+        marginRight: 0,
+        marginBottom: 0,
+        marginLeft: 0,
+      },
+    },
+  });
+
+  return (
+    <BaseBanner {...rest} overrides={mergedOverrides}>
+      {children}
+    </BaseBanner>
+  );
+};
