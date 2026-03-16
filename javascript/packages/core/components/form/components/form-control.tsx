2ed7307560ebb5b238c256fc09bbd785abe24ab2
Selim Konuk <selim.konuk@uber.com>
2026-03-16 11:37:04 +0100

    Support markdown in form-control captions

--- javascript/packages/core/components/form/components/form-control.tsx
+++ javascript/packages/core/components/form/components/form-control.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useStyletron } from 'baseui';
 import { FormControl as BaseFormControl } from 'baseui/form-control';
 
+import { Markdown } from '#core/components/markdown/markdown';
+import { TruncatedText } from '#core/components/truncated-text/truncated-text';
 import { Label } from './label/label';
 
 import type { FormControlProps } from './types';
@@ -21,7 +23,13 @@ export const FormControl: React.FC<FormControlProps> = ({
     <div className={css({ width: '100%' })}>
       <BaseFormControl
         label={label && <Label label={label} required={required} description={description} />}
-        caption={caption}
+        caption={
+          caption && (
+            <TruncatedText>
+              <Markdown>{caption}</Markdown>
+            </TruncatedText>
+          )
+        }
         counter={counter}
         error={error}
         overrides={{
@@ -31,6 +39,11 @@ export const FormControl: React.FC<FormControlProps> = ({
               // provided by FormControl default interferes with form layout spacing.
               marginBottom: 0,
             },
+          },
+          Caption: {
+            style: ({ $error, $positive }) => ({
+              display: $error || $positive ? 'flex' : 'block',
+            }),
           },
         }}
       >
